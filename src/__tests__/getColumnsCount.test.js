234cead20ef58f10cad5760a39123d88f441cfb5
Adrian Smijulj <adrian1358@gmail.com>
2019-05-31 09:29:47 +0200

    chore: test stubs

--- src/__tests__/getColumnsCount.test.js
+++
@@ -1,5 +0,0 @@
-describe("getColumnsCount test", () => {
-    test("if integer passed, that means columns count is hard-coded, and we just return it", () => {
-        expect(4).toBe(4);
-    });
-});
